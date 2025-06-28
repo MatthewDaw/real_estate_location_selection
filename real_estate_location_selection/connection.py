03547913a45af74d4aea0f36a4c2204c0fb30ab6
Matthew Daw <matthewdaw@Mac.lan>
2025-06-28 10:24:56 -0600

    try again

--- real_estate_location_selection/connection.py
+++ real_estate_location_selection/connection.py
@@ -1,22 +1,6 @@
 import psycopg
 import os
 
-def hd_connection():
-    """
-    Lazily initializes a database connection.
-
-    Ensures a fresh connection is created only when needed,
-    reducing the risk of connection loss during long-running pipeline operations.
-    """
-    return psycopg.connect(
-        port="5432",
-        host=os.getenv("INSTANCE_UNIX_SOCKET") or os.getenv("DB_HOST")[:-5],
-        dbname=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASS"),
-        application_name="update-core",
-    )
-
 def local_db_connection():
     """
     Lazily initializes a database connection.
