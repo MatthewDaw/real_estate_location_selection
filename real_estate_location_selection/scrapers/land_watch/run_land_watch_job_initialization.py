9a94408c23bcaac11d8f50fa4b64344302cee4eb
Matthew Daw <matthewdaw@Mac.lan>
2025-07-06 10:19:45 -0600

    fixing stuff

--- real_estate_location_selection/scrapers/land_watch/run_land_watch_job_initialization.py
+++ real_estate_location_selection/scrapers/land_watch/run_land_watch_job_initialization.py
@@ -117,9 +117,8 @@ def run_scraper():
     print("scraper started")
     browser = get_browser()
     scraper = Landwatch(browser)
-    scraper._fetch_urls_to_scrape()
     print("preparing tasks")
-    # prepare_tasks(scraper)
+    prepare_tasks(scraper)
 
 if __name__ == '__main__':
     run_scraper()
