a91a4a22e1f6950664cdd29353199fc350fc4f5e
Matthew Daw <matthewdaw@Matthews-MacBook-Air.local>
2025-07-11 02:23:10 -0600

    please

--- start_scraping.py
+++ start_scraping.py
@@ -260,7 +260,7 @@ def main():
     # Configuration
     PROJECT_ID = "flowing-flame-464314-j5"
     LANDWATCH_TASK_COUNT = 24  # Number of LandWatch tasks to distribute
-    ZILLOW_TASK_COUNT = 32  # Number of Zillow tasks to distribute
+    ZILLOW_TASK_COUNT = 24  # Number of Zillow tasks to distribute
     PROVISIONING_MODEL = "SPOT"  # Options: "STANDARD" or "SPOT"
     MAX_TASKS_PER_REGION = 8  # Maximum tasks per region to ensure we stay within quota
     USE_QUOTA_CHECKING = True  # Set to False if you don't have google-cloud-compute installed
