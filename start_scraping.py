d155e3a5cf78f56e4d01df3852e2ee0040e1327a
Matthew Daw <matthewdaw@Matthews-MacBook-Air.local>
2025-07-11 08:53:26 -0600

    changing zillow batch size

--- start_scraping.py
+++ start_scraping.py
@@ -259,7 +259,7 @@ class UnifiedScraperDispatcher:
 def main():
     # Configuration
     PROJECT_ID = "flowing-flame-464314-j5"
-    LANDWATCH_TASK_COUNT = 24  # Number of LandWatch tasks to distribute
+    LANDWATCH_TASK_COUNT = 0  # Number of LandWatch tasks to distribute
     ZILLOW_TASK_COUNT = 24  # Number of Zillow tasks to distribute
     PROVISIONING_MODEL = "SPOT"  # Options: "STANDARD" or "SPOT"
     MAX_TASKS_PER_REGION = 8  # Maximum tasks per region to ensure we stay within quota
