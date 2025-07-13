e80da0195b01958300a0dbde43d47788aa49133c
Matthew Daw <matthewdaw@Matthews-MacBook-Air.local>
2025-07-13 00:53:35 -0600

    try again

--- src/real_estate_location_selection/landwatch.py
+++
@@ -1,9 +0,0 @@
-from camoufox.sync_api import Camoufox
-from real_estate_location_selection.scrapers.utils.common_functions import get_browser
-
-def run_scrape():
-    scraper = ApartmentListScraper(browser)
-    details = scraper.process_tasks("p31370")
-
-
-
--- src/real_estate_location_selection/scrapers/land_watch/load_landwatch_queue.py
+++
@@ -1,6 +0,0 @@
-from real_estate_location_selection.load_pub_sub import load_jobs_into_pub_sub
-
-if __name__ == '__main__':
-    # job_loader = DistributedJobLoader("flowing-flame-464314-j5", "landwatch",
-    #                                   dataset_id="real_estate")
-    load_jobs_into_pub_sub("landwatch",1000)
